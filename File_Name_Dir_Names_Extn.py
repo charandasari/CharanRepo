99d10644eae14e9ba999b8214693c2307c7eb403
charandasari <56388170+charandasari@users.noreply.github.com>
2019-10-11 20:23:33 +0530

    Added Create Directory Func Def

--- File_Name_Dir_Names_Extn.py
+++ File_Name_Dir_Names_Extn.py
@@ -1,9 +1,13 @@
 import os
+import shutil
 import pprint
 filenames=[]
 dirnames=[]
 extn=[]
 count={}
+
+#input_frm_user=input("Please enter a directory path make sure to provide \\ : ")
+
 
 os.chdir(r'C:\Users\charan.teja\Downloads')#Directory switch to Downloads
 
@@ -15,20 +19,33 @@ def dir_names():            #Function to list directory names
 def file_names():           #Function to list filename in a given directory
   for filename in os.listdir():
      if os.path.isfile(filename):
+        filenames.append(filename)
         name,file_extension = os.path.splitext(os.path.join(r'C:\Users\charan.teja\Downloads',filename))#To split filename and extension and store extension in an empty list
         extn.append(file_extension)
-        filenames.append(filename)
+        
 
 def count_type():           #Function to count number of files with each extension
    for item in extn:
      count.setdefault(item,0)
-     count[item]=count[item]+1 
-        
+     count[item]=count[item]+1
+
+def create_dir():           #Function to create directories based on file extensions
+    for item in count:
+        try:
+            os.makedirs(item[1:])
+        except FileExistsError:
+            print('Directory alredy exsists : ' + item[1:])
+
+
+
 #Below are function calls for above functions
      
 dir_names()
 file_names()
 count_type()
+create_dir()
+
+
 
 print('List of Directories in Downloads'.center(100,'='))#provides output like =======Text========
 pprint.pprint(dirnames)
